da854f3c5837d8b1298d81799bb234ffd985a58b
lwzhang <lwzhang@thoughtworks.com>
2017-10-11 16:17:48 +0800

    Initial Commit

---
+++ CatOnCloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatOnCloud
+//
+//  Created by Liwen Zhang on 11/10/2017.
+//  Copyright © 2017 Liwen Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
